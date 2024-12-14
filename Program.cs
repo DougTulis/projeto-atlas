0f939b7cf951bdb68fecbba3ca107c91dee30070
Douglas <dengratulis@gmail.com>
2024-12-14 13:24:05 -0300

    Criando o esqueleto do menu principal

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
 
-        public void MostrarLogo()
+        static void MostrarLogo()
         {
             Console.WriteLine(@"
  █████╗ ████████╗██╗      █████╗ ███████╗
@@ -16,6 +16,7 @@
 
         static void MostrarMenu()
         {
+            MostrarLogo();
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Escolha uma opção: ");
@@ -23,6 +24,8 @@
             Console.WriteLine("2. Locações");
             Console.WriteLine("3. Financeiro ");
             Console.WriteLine("4. Sair");
+            int Escolha = int.Parse(Console.ReadLine());
+            ExecutarEscolha(Escolha);
         }
 
         static void ExecutarEscolha(int Escolha)
@@ -31,27 +34,50 @@
             {
                 case 1:
                     // MenuPessoas();
+                    Console.Clear();
+                    Console.WriteLine("Executando menu pessoas");
+                    Console.ReadKey();
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    MostrarMenu();
                     break;
                 case 2:
                     // MenuLocacoes();
+                    Console.Clear();
+                    Console.WriteLine("Executando menu Locações");
+                    Console.ReadKey();
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    MostrarMenu();
+
                     break;
                 case 3:
                     // Financeiro();
+                    Console.Clear();
+                    Console.WriteLine("Executando menu Financeiro");
+                    Console.ReadKey();
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    MostrarMenu();
                     break;
                 case 4:
                     // Sair();
+                    Console.Clear();
+                    Console.WriteLine("Volte Logo!! :)");
+                    return;
+                    MostrarMenu();
                     break;
                 default:
                     Console.WriteLine("Opção invalida");
                     break;
+
             }
 
         }
 
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MostrarMenu();
         }
     }
 }
